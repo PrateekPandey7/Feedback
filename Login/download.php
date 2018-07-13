97a4c56fb11dea4a7791302bc8eb4a23a7ca8c93
PrateekPandey7 <pandeyprateek01@gmail.com>
2018-07-14 00:44:26 +0530

    changes

--- Login/download.php
+++ Login/download.php
@@ -19,7 +19,7 @@
 	   	<link href="https://fonts.googleapis.com/css?family=EB+Garamond" rel="stylesheet">
 
 
-	    <title>Confirmation Page</title>
+	    <title>Download Page</title>
 
 	</head>
   
@@ -27,34 +27,30 @@
 
 		<div class = "mainHeading">
 			<img src = "../npc.jpg">
-			<p class = "heading1"><strong>Coordinator Control Panel</strong><p>
-			<!--<p class = "subh1">(Under Department of Industrial Policy & Promotion Ministry of Commerce & Industry, Govt. of India)</p>-->
+			<p class = "heading1"><strong>National Productivity Council</strong><p>
+			<p class = "subh1">(Under Department of Industrial Policy & Promotion Ministry of Commerce & Industry, Govt. of India)</p>
+			<p class = "heading2">FEEDBACK FORM</p>
+			<p class = "heading3">Control Panel</p>
 		</div>
 
 		<div class = "information">
 			<div class = "instruction">
-				<p><strong><center>Instructions</center></strong></p>
-				<p>1) Provide the passcode to the participant to fill the feedback forms. The passcode is : <strong><?php echo $code ?></strong></p>
-				<p>2)To download the generated data in excel format click on <strong>"Download Feedback Excel"</strong> button
-					<div class="wrapper">
-  					<span class="square individual">
-    				<a class = "link" <?php echo "href=../Excel/downloadExcel.php?name=".urlencode($file)?> target = '_blank'>Download Feedback Excel Report</a>
-  					</span>
-				</div>
+				<p><strong>How to obtain feedback from participants?</strong></p>
+				<p>1) Please ask the participants to open Online Feedback Form in the browser of their mobile phone/computer.</p>
+				<p>2) Please share the Passcode: <strong><?php echo $code ?></strong> that is generated for your program with the participants to login to the Online Feedback Form.
+				<p>3) You may request the participants to submit their Feedback of the program attended.</p>
+				<br>
+				<p><strong>How to generate reports?</strong></p>
+				<p>1) To download the <strong>Summary Report</strong> of all feedbacks, please
+    				<a class = "link" <?php echo "href=../Excel/downloadExcel.php?name=".urlencode($file)?> target = '_blank'>Click Here</a>
 				</p>
-				<p>3)To download the generated Reports of participants click on <strong>"Download Feedback PDF Reports"</strong> button
-					<div class="wrapper">
-  					<span class="square individual">
-    				<a class = "link" <?php echo "href=../FeedbackForms/zip_folders.php?name=".urlencode($string)?> target = '_blank'>Download Feedback PDF Reports</a>
-  					</span>
-				</div>
+				<p>2) To download filled <strong>Feedback Forms</strong> of all participants, please
+    				<a class = "link" <?php echo "href=../FeedbackForms/zip_folders.php?name=".urlencode($string)?> target = '_blank'>Click Here</a>
 				</p>
-				<p>4)After collecting and download all the feedback forms click on <strong>"Delete this Account"</strong>
-					<div class="wrapper">
-  					<span class="square individual">
-    				<a class = "link" <?php echo "href=logout.php?name=".urlencode($token)?>>Delete This Account</a>
-  					</span>
-				</div>
+				<br>
+				<p><strong>How to end the session?</strong></p>
+				<p>1) To delete the passcode and logout, please
+					<a class = "link" <?php echo "href=logout.php?name=".urlencode($token)?>>Click Here</a>
 				</p> 
 			</div>
 		</div>
